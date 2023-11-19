520b6061f7de2f51cfd5fdd73464542c76777e21
Guuuti <126295468+Guuuti@users.noreply.github.com>
2023-11-19 20:08:16 -0300

    Mensage - Index/Component

--- my-app/src/components/Commitment/index.tsx
+++ my-app/src/components/Commitment/index.tsx
@@ -1,18 +1,26 @@
 import { View, Text } from "react-native";
 import { MyStyles } from "./styles";
 
-export function Commitment({navigation}) {
+type CommitmentProps = {
+  usuario: string;
+  agenda: string;
+  dia: string;
+  horaInicio: string;
+  horaFim: string;
+}
+
+export function Commitment(props: CommitmentProps) {
   const styles = MyStyles();
 
   return (
     <View style={styles.mainContainer}>
       <View style={styles.subContainer}>
-        <Text style={styles.textOne}>Nome do Cliente</Text>
-        <Text style={styles.textTwo}>Nome da Agenda</Text>
+        <Text style={styles.textOne}>{props.usuario}</Text>
+        <Text style={styles.textTwo}>{props.agenda}</Text>
       </View>
       <View style={styles.subContainer}>
-        <Text style={styles.textOne}>Dia Marcado</Text>
-        <Text style={styles.textTwo}>Hora Marcada</Text>
+        <Text style={styles.textOne}>{props.dia}</Text>
+        <Text style={styles.textTwo}>{`${props.horaInicio} - ${props.horaFim}`}</Text>
       </View>
     </View>
   );
